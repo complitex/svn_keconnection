c54f5f2d1d9b8b463e33b8af746a696f44b69f64
heaven.iaa <heaven.iaa@f1f58e75-00e4-d601-798c-4044314edfc3>
2012-09-13 11:31:43 +0000

    change heatmeater db

--- keconnection-db/keconnection-create.sql
+++ keconnection-db/keconnection-create.sql
@@ -8,32 +8,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- ------------------------------
--- Heatmeater
--- ------------------------------
-
-DROP TABLE IF EXISTS `heatmeater`;
-CREATE TABLE `heatmeater`(
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор',
-  `gek` INT(11) NOT NULL COMMENT 'Код структурной единицы',
-  `dom` INT(11) NOT NULL COMMENT 'Код дома',
-  `ul` VARCHAR(255) NOT NULL COMMENT 'Название улицы вместе с типом',
-  `ndom` VARCHAR(10) NOT NULL COMMENT 'Номер дома',
-  `lotop0` INT(11) COMMENT 'Номер л/с первого счетчика в доме',
-  `lotop1` INT(11) COMMENT 'Номер л/с второго счетчика в доме',
-  `lotop2` INT(11) COMMENT 'Номер л/с третьего счетчика в доме',
-  `lotop3` INT(11) COMMENT 'Номер л/с четвертого счетчика в доме',
-  `lotop4` INT(11) COMMENT 'Номер л/с пятого счетчика в доме',
-  `organization_id` BIGINT(20) COMMENT 'Идентификатор структурной единицы',
-  `building_id` BIGINT(20) COMMENT 'Идентификатор дома',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `heatmeater_unique_id` (`gek`, `dom`),
-  KEY `key_organization_id` (`organization_id`),
-  KEY `ke_building_id` (`building_id`),
-  CONSTRAINT `fk_heatmeater__organization` FOREIGN KEY (`organization_id`) REFERENCES `organization` (`object_id`),
-  CONSTRAINT `fk_heatmeater__building` FOREIGN KEY (`building_id`) REFERENCES `building` (`object_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Теплосчетчик';
-
--- ------------------------------
 -- Building organization association
 -- ------------------------------
 
@@ -49,6 +23,36 @@ CREATE TABLE `building_code` (
   CONSTRAINT `fk_building_code__organization` FOREIGN KEY (`organization_id`) REFERENCES `organization` (`object_id`),
   CONSTRAINT `fk_building_code__building` FOREIGN KEY (`building_id`) REFERENCES `building` (`object_id`)
 ) ENGINE=InnoDB DEFAULT  CHARSET=utf8 COMMENT 'Код дома';
+
+-- ------------------------------
+-- Heatmeater Type
+-- ------------------------------
+
+DROP TABLE IF EXISTS `heatmeater_type`;
+CREATE TABLE `heatmeater_type`(
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор',
+  `name` VARCHAR(255) NOT NULL COMMENT 'Название типа теплосчетчика'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Тип теплосчетчика';
+
+-- ------------------------------
+-- Heatmeater
+-- ------------------------------
+
+DROP TABLE IF EXISTS `heatmeater`;
+CREATE TABLE `heatmeater`(
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор',
+  `ls` INT(7) NOT NULL COMMENT 'Номер л/с теплосчетчика',
+  `building_code_id` BIGINT(20) NOT NULL COMMENT 'Ссылка на код дома',
+  `type_id` BIGINT(20) NOT NULL COMMENT 'Ссылка на тип счетчика',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `heatmeater_unique_id` (`ls`, `building_code_id`),
+  KEY `key_ls` (`ls`),
+  KEY `key_building_code_id` (`building_code_id`),
+  CONSTRAINT `fk_heatmeater__building_code` FOREIGN KEY (`building_code_id`) REFERENCES `building_code` (`id`),
+  CONSTRAINT `fk_heatmeater__heatmeater_type` FOREIGN KEY (`type_id`) REFERENCES `heatmeater_type` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Теплосчетчик';
+
+
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
