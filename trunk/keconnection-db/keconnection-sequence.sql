7353d8cb9194c492c9b0fbc4b7788c87c37a0e5b
artkof.work <artkof.work@f1f58e75-00e4-d601-798c-4044314edfc3>
2012-09-19 09:00:36 +0000

    Support of tarif groups has been added.

--- trunk/keconnection-db/keconnection-sequence.sql
+++ trunk/keconnection-db/keconnection-sequence.sql
@@ -4,3 +4,5 @@ update sequence set sequence_value = (select IFNULL(max(`id`), 0) from `organiza
 update sequence set sequence_value = (select IFNULL(max(`object_id`), 0) from `organization`)+1 where sequence_name = 'organization';
 update sequence set sequence_value = (select IFNULL(max(`id`), 0) from `organization_type_string_culture`)+1 where sequence_name = 'organization_type_string_culture';
 update sequence set sequence_value = (select IFNULL(max(`object_id`), 0) from `organization_type`)+1 where sequence_name = 'organization_type';
+update sequence set sequence_value = (select IFNULL(max(`id`), 0) from `tarif_group_string_culture`)+1 where sequence_name = 'tarif_group_string_culture';
+update sequence set sequence_value = (select IFNULL(max(`object_id`), 0) from `tarif_group`)+1 where sequence_name = 'tarif_group';
