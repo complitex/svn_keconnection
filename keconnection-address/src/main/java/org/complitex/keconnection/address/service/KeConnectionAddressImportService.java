15de17c1eae95e9013e80e19d415605cc0a0f814
artkof.work <artkof.work@f1f58e75-00e4-d601-798c-4044314edfc3>
2012-09-25 05:10:39 +0000

    Support of import warning messages as some kind of non-fatal errors has been added to core complitex address import mechanism. The means of import warnings have been applied to import of streets and buildings in keconnection project.

--- keconnection-address/src/main/java/org/complitex/keconnection/address/service/KeConnectionAddressImportService.java
+++ keconnection-address/src/main/java/org/complitex/keconnection/address/service/KeConnectionAddressImportService.java
@@ -4,14 +4,17 @@
  */
 package org.complitex.keconnection.address.service;
 
+import org.complitex.dictionary.util.CloneUtil;
+import org.complitex.address.strategy.district.DistrictStrategy;
+import org.complitex.address.strategy.street.StreetStrategy;
 import au.com.bytecode.opencsv.CSVReader;
 import com.google.common.base.Strings;
 import org.complitex.address.entity.AddressImportFile;
 import org.complitex.address.service.AddressImportService;
 import org.complitex.address.strategy.building.BuildingStrategy;
 import org.complitex.address.strategy.building_address.BuildingAddressStrategy;
-import org.complitex.address.strategy.district.DistrictStrategy;
-import org.complitex.address.strategy.street.StreetStrategy;
+import org.complitex.address.strategy.city.CityStrategy;
+import org.complitex.address.strategy.street_type.StreetTypeStrategy;
 import org.complitex.dictionary.entity.Attribute;
 import org.complitex.dictionary.entity.DomainObject;
 import org.complitex.dictionary.service.AbstractImportService;
@@ -24,6 +27,7 @@ import org.complitex.dictionary.service.exception.ImportObjectLinkException;
 import org.complitex.dictionary.util.AttributeUtil;
 import org.complitex.dictionary.util.BuildingNumberConverter;
 import org.complitex.dictionary.util.DateUtil;
+import org.complitex.dictionary.util.ResourceUtil;
 import org.complitex.keconnection.address.strategy.building.KeConnectionBuildingStrategy;
 import org.complitex.keconnection.address.strategy.building.entity.BuildingOrganizationAssociation;
 import org.complitex.keconnection.address.strategy.building.entity.KeConnectionBuilding;
@@ -35,9 +39,8 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import java.io.IOException;
 
-import static org.complitex.address.entity.AddressImportFile.BUILDING;
-import static org.complitex.dictionary.util.StringUtil.removeWhiteSpaces;
-import static org.complitex.dictionary.util.StringUtil.toCyrillic;
+import static org.complitex.address.entity.AddressImportFile.*;
+import static org.complitex.dictionary.util.StringUtil.*;
 
 /**
  *
@@ -47,6 +50,7 @@ import static org.complitex.dictionary.util.StringUtil.toCyrillic;
 public class KeConnectionAddressImportService extends AbstractImportService {
 
     private static final Logger log = LoggerFactory.getLogger(KeConnectionAddressImportService.class);
+    private static final String RESOURCE_BUNDLE = KeConnectionAddressImportService.class.getName();
     @EJB
     private AddressImportService addressImportService;
     @EJB
@@ -59,6 +63,10 @@ public class KeConnectionAddressImportService extends AbstractImportService {
     private KeConnectionBuildingStrategy buildingStrategy;
     @EJB(name = IKeConnectionOrganizationStrategy.KECONNECTION_ORGANIZATION_STRATEGY_NAME)
     private IKeConnectionOrganizationStrategy organizationStrategy;
+    @EJB
+    private CityStrategy cityStrategy;
+    @EJB
+    private StreetTypeStrategy streetTypeStrategy;
 
     public void process(AddressImportFile addressFile, IImportListener listener, long localeId)
             throws ImportFileNotFoundException, ImportFileReadException, ImportObjectLinkException, ImportDuplicateException {
@@ -82,7 +90,7 @@ public class KeConnectionAddressImportService extends AbstractImportService {
                 addressImportService.importStreetType(listener, localeId);
                 break;
             case STREET:
-                addressImportService.importStreet(listener, localeId);
+                importStreet(listener, localeId);
                 break;
             case BUILDING:
                 importBuilding(listener, localeId);
@@ -126,7 +134,10 @@ public class KeConnectionAddressImportService extends AbstractImportService {
 
                 Long streetId = streetStrategy.getObjectId(streetExternalId);
                 if (streetId == null) {
-                    throw new ImportObjectLinkException(BUILDING.getFileName(), recordIndex, line[2]);
+                    listener.warn(BUILDING, ResourceUtil.getFormatString(RESOURCE_BUNDLE, "building_street_not_found_warn",
+                            localeBean.getLocale(localeId),
+                            line[3] + " " + line[4], line[0], streetExternalId));
+                    continue;
                 }
 
                 final String number = prepareBuildingNumber(recordIndex, line[3]);
@@ -206,6 +217,100 @@ public class KeConnectionAddressImportService extends AbstractImportService {
             throw new ImportFileReadException(e, BUILDING.getFileName(), recordIndex);
         } catch (NumberFormatException e) {
             throw new ImportFileReadException(e, BUILDING.getFileName(), recordIndex);
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                log.error("Ошибка закрытия потока", e);
+            }
+        }
+    }
+
+    /**
+     * STREET_ID	CITY_ID	STREET_TYPE_ID	Название улицы
+     * @throws ImportFileNotFoundException
+     * @throws ImportFileReadException
+     */
+    public void importStreet(IImportListener listener, long localeId)
+            throws ImportFileNotFoundException, ImportFileReadException, ImportObjectLinkException, ImportDuplicateException {
+        listener.beginImport(STREET, getRecordCount(STREET));
+
+        CSVReader reader = getCsvReader(STREET);
+
+        final long systemLocaleId = localeBean.getSystemLocaleObject().getId();
+        int recordIndex = 0;
+
+        try {
+            String[] line;
+
+
+            while ((line = reader.readNext()) != null) {
+                recordIndex++;
+
+                final long externalId = Long.parseLong(line[0].trim());
+
+                DomainObject newObject = null;
+                DomainObject oldObject = null;
+
+                // Ищем по externalId в базе.
+                Long objectId = streetStrategy.getObjectId(externalId);
+                if (objectId != null) {
+                    oldObject = streetStrategy.findById(objectId, true);
+                    if (oldObject != null) {
+                        newObject = CloneUtil.cloneObject(oldObject);
+                    }
+                }
+                if (newObject == null) {
+                    newObject = streetStrategy.newInstance();
+                    newObject.setExternalId(externalId);
+                }
+
+                //name
+                final String name = line[3].trim().toUpperCase();
+                AttributeUtil.setStringValue(newObject.getAttribute(StreetStrategy.NAME), name, localeId);
+                if (AttributeUtil.getSystemStringCultureValue(newObject.getAttribute(StreetStrategy.NAME)) == null) {
+                    AttributeUtil.setStringValue(newObject.getAttribute(StreetStrategy.NAME), name, systemLocaleId);
+                }
+
+                //CITY_ID
+                Long cityId = cityStrategy.getObjectId(Long.parseLong(line[1].trim()));
+                if (cityId == null) {
+                    throw new ImportObjectLinkException(STREET.getFileName(), recordIndex, line[1]);
+                }
+                newObject.setParentEntityId(StreetStrategy.PARENT_ENTITY_ID);
+                newObject.setParentId(cityId);
+
+                //STREET_TYPE_ID
+                Long streetTypeId = streetTypeStrategy.getObjectId(Long.parseLong(line[2].trim()));
+                if (streetTypeId == null) {
+                    throw new ImportObjectLinkException(STREET.getFileName(), recordIndex, line[2]);
+                }
+                newObject.getAttribute(StreetStrategy.STREET_TYPE).setValueId(streetTypeId);
+
+                // сначала ищем улицу в системе с таким названием, типом и родителем(городом)
+                final Long existingStreetId = streetStrategy.performDefaultValidation(newObject, localeBean.getSystemLocale());
+                if (existingStreetId != null) {
+                    // нашли дубликат
+                    DomainObject existingStreet = streetStrategy.findById(existingStreetId, true);
+                    long existingStreetExternalId = existingStreet.getExternalId();
+                    listener.warn(STREET, ResourceUtil.getFormatString(RESOURCE_BUNDLE, "street_duplicate_warn",
+                            localeBean.getLocale(localeId),
+                            line[3], externalId, existingStreetId, existingStreetExternalId));
+                } else {
+                    if (oldObject == null) {
+                        streetStrategy.insert(newObject, DateUtil.getCurrentDate());
+                    } else {
+                        streetStrategy.update(oldObject, newObject, DateUtil.getCurrentDate());
+                    }
+                    listener.recordProcessed(STREET, recordIndex);
+                }
+            }
+
+            listener.completeImport(STREET, recordIndex);
+        } catch (IOException e) {
+            throw new ImportFileReadException(e, STREET.getFileName(), recordIndex);
+        } catch (NumberFormatException e) {
+            throw new ImportFileReadException(e, STREET.getFileName(), recordIndex);
         } finally {
             try {
                 reader.close();
