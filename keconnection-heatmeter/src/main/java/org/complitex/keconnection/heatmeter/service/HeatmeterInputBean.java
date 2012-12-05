5fcc6b6576654e3e11083e29d1d5ddc12c31c7d7
artkof.work <artkof.work@f1f58e75-00e4-d601-798c-4044314edfc3>
2012-12-05 11:10:40 +0000

    Consumption calculations has been added to heatmeter edit page.

--- keconnection-heatmeter/src/main/java/org/complitex/keconnection/heatmeter/service/HeatmeterInputBean.java
+++ keconnection-heatmeter/src/main/java/org/complitex/keconnection/heatmeter/service/HeatmeterInputBean.java
@@ -1,26 +1,17 @@
 package org.complitex.keconnection.heatmeter.service;
 
-import com.google.common.base.Predicate;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
 import org.complitex.dictionary.mybatis.Transactional;
 import org.complitex.dictionary.mybatis.XmlMapper;
 import org.complitex.keconnection.heatmeter.entity.HeatmeterConsumption;
 import org.complitex.keconnection.heatmeter.entity.HeatmeterInput;
-import org.complitex.keconnection.heatmeter.entity.HeatmeterPayload;
 import org.complitex.keconnection.heatmeter.entity.HeatmeterPeriodType;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import java.math.BigDecimal;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
 
 import static com.google.common.collect.ImmutableMap.of;
-import static org.complitex.dictionary.util.DateUtil.getDaysDiff;
-import static org.complitex.dictionary.util.DateUtil.getMin;
 import static org.complitex.keconnection.heatmeter.entity.HeatmeterPeriodType.INPUT;
 
 /**
@@ -62,72 +53,5 @@ public class HeatmeterInputBean extends HeatmeterPeriodBean<HeatmeterInput> {
 
     public List<HeatmeterInput> getHeatmeterInputs(Long heatmeterId, Date om) {
         return sqlSession().selectList("selectHeatmeterInputsByOm", of("heatmeterId", heatmeterId, "om", om));
-    }
-
-    //todo move to HeatmeterInput/ConsumptionService
-    public void calculateConsumptions(Iterable<HeatmeterPayload> payloads, Iterable<HeatmeterInput> inputs) {
-        //filter out non existing payloads
-        List<HeatmeterPayload> payloadsList = Lists.newArrayList(Iterables.filter(payloads, new Predicate<HeatmeterPayload>() {
-
-            @Override
-            public boolean apply(HeatmeterPayload payload) {
-                return payload.getId() != null;
-            }
-        }));
-
-        //sort payload in ascending order by begin date
-        Collections.sort(payloadsList, new Comparator<HeatmeterPayload>() {
-
-            @Override
-            public int compare(HeatmeterPayload p1, HeatmeterPayload p2) {
-                return p1.getBeginDate().compareTo(p2.getBeginDate());
-            }
-        });
-
-        for (HeatmeterInput input : inputs) {
-            calculateConsumption(payloadsList, input);
-        }
-    }
-
-    private void calculateConsumption(List<HeatmeterPayload> payloads, HeatmeterInput input) {
-        HeatmeterConsumption c = input.getFirstConsumption();
-        if (c.getId() == null) { // new consumption
-            c.setBeginDate(input.getBeginDate());
-            c.setEndDate(input.getEndDate());
-            c.setOm(input.getEndOm());
-        }
-        c.setConsumption1(BigDecimal.ZERO);
-        c.setConsumption2(BigDecimal.ZERO);
-        c.setConsumption3(BigDecimal.ZERO);
-
-        int inputPeriodLength = getDaysDiff(input.getBeginDate(), input.getEndDate());
-
-        if (inputPeriodLength != 0) {
-            for (HeatmeterPayload p : payloads) {
-                if (!p.getBeginDate().after(input.getBeginDate())
-                        && p.getEndDate().after(input.getBeginDate())) {
-                    Date subPeriodEndDate = getMin(p.getEndDate(), input.getEndDate());
-                    int subPeriodLength = getDaysDiff(input.getBeginDate(), subPeriodEndDate);
-                    addSubPeriodCalculations(input.getValue(), inputPeriodLength, subPeriodLength, c,
-                            p.getPayload2(), p.getPayload3());
-                } else if (p.getBeginDate().after(input.getBeginDate())
-                        && p.getBeginDate().before(input.getEndDate())) {
-                    Date subPeriodEndDate = getMin(p.getEndDate(), input.getEndDate());
-                    int subPeriodLength = getDaysDiff(p.getBeginDate(), subPeriodEndDate);
-                    addSubPeriodCalculations(input.getValue(), inputPeriodLength, subPeriodLength, c,
-                            p.getPayload2(), p.getPayload3());
-                }
-            }
-            c.setConsumption1(input.getValue().subtract(c.getConsumption2()).subtract(c.getConsumption3()));
-        }
-    }
-
-    private void addSubPeriodCalculations(BigDecimal value, int periodLength, int subPeriodLength,
-            HeatmeterConsumption consumption, BigDecimal payload2, BigDecimal payload3) {
-        consumption.setConsumption2(consumption.getConsumption2().add(BigDecimal.valueOf(
-                (value.doubleValue() * subPeriodLength * payload2.doubleValue()) / (periodLength * 100))));
-
-        consumption.setConsumption3(consumption.getConsumption3().add(BigDecimal.valueOf(
-                (value.doubleValue() * subPeriodLength * payload3.doubleValue()) / (periodLength * 100))));
     }
 }
--- keconnection-heatmeter/src/main/java/org/complitex/keconnection/heatmeter/web/HeatmeterEdit.java
+++ keconnection-heatmeter/src/main/java/org/complitex/keconnection/heatmeter/web/HeatmeterEdit.java
@@ -38,7 +38,7 @@ import javax.ejb.EJB;
 import java.util.Date;
 import java.util.List;
 
-import static org.complitex.dictionary.util.DateUtil.addMonth;
+import static org.complitex.dictionary.util.DateUtil.*;
 import static org.complitex.keconnection.heatmeter.entity.HeatmeterValidateStatus.VALID;
 import static org.complitex.keconnection.organization.strategy.IKeConnectionOrganizationStrategy.KE_ORGANIZATION_OBJECT_ID;
 
@@ -47,21 +47,17 @@ import static org.complitex.keconnection.organization.strategy.IKeConnectionOrga
  *         Date: 04.09.12 15:25
  */
 @AuthorizeInstantiation(SecurityRole.ADMIN_MODULE_EDIT)
-public class HeatmeterEdit extends FormTemplatePage{
-    private final static Logger log = LoggerFactory.getLogger(HeatmeterEdit.class);
+public class HeatmeterEdit extends FormTemplatePage {
 
+    private final static Logger log = LoggerFactory.getLogger(HeatmeterEdit.class);
     @EJB
     private HeatmeterBean heatmeterBean;
-
     @EJB
     private HeatmeterService heatmeterService;
-
     @EJB(name = IKeConnectionOrganizationStrategy.KECONNECTION_ORGANIZATION_STRATEGY_NAME)
     private IKeConnectionOrganizationStrategy organizationStrategy;
-
     @EJB
     private KeConnectionBuildingStrategy buildingStrategy;
-
     private IModel<Date> om = new Model<>();
     private Date minOm = null;
 
@@ -73,18 +69,17 @@ public class HeatmeterEdit extends FormTemplatePage{
         init(pageParameters.get("id").toLongObject());
     }
 
-    private void init(Long id){
+    private void init(Long id) {
         add(new Label("title", new ResourceModel("title")));
         add(new FeedbackPanel("messages"));
 
         Heatmeter heatmeter;
 
-        if (id != null){
+        if (id != null) {
             heatmeter = heatmeterBean.getHeatmeter(id);
             om.setObject(heatmeter.getOm());
             minOm = heatmeterBean.getMinOm(heatmeter.getId());
-        }
-        else{
+        } else {
             heatmeter = new Heatmeter();
             heatmeter.setOrganizationId(KE_ORGANIZATION_OBJECT_ID);
         }
@@ -108,7 +103,8 @@ public class HeatmeterEdit extends FormTemplatePage{
         form.add(container);
 
         //Operating month
-        final WebMarkupContainer omContainer = new WebMarkupContainer("om_container"){
+        final WebMarkupContainer omContainer = new WebMarkupContainer("om_container") {
+
             @Override
             public boolean isVisible() {
                 return om.getObject() != null;
@@ -120,6 +116,7 @@ public class HeatmeterEdit extends FormTemplatePage{
         omContainer.add(new Label("current_operation_month", om));
 
         omContainer.add(new AjaxLink("previous_month") {
+
             @Override
             public void onClick(AjaxRequestTarget target) {
                 om.setObject(addMonth(om.getObject(), -1));
@@ -134,6 +131,7 @@ public class HeatmeterEdit extends FormTemplatePage{
         });
 
         omContainer.add(new AjaxLink("next_month") {
+
             @Override
             public void onClick(AjaxRequestTarget target) {
                 om.setObject(addMonth(om.getObject(), 1));
@@ -160,7 +158,8 @@ public class HeatmeterEdit extends FormTemplatePage{
         container.add(new HeatmeterInputPanel("inputs", model, om));
 
         //Save
-        form.add(new Button("save"){
+        form.add(new Button("save") {
+
             @Override
             public void onSubmit() {
                 try {
@@ -169,20 +168,20 @@ public class HeatmeterEdit extends FormTemplatePage{
                     //validate
                     HeatmeterValidate validate = heatmeterService.validate(heatmeter);
 
-                    if (!VALID.equals(validate.getStatus())){
+                    if (!VALID.equals(validate.getStatus())) {
                         error(getStringFormat(validate.getStatus().name().toLowerCase(), validate));
 
                         return;
                     }
 
                     //update om for new heatmeter
-                    if (heatmeter.getId() == null){
+                    if (heatmeter.getId() == null) {
                         HeatmeterConnection connection = heatmeter.getConnections().get(0);
                         BuildingCode buildingCode = buildingStrategy.getBuildingCodeById(connection.getObjectId());
 
                         Date om = organizationStrategy.getOperatingMonthDate(buildingCode.getOrganizationId());
 
-                        if (om == null){
+                        if (om == null) {
                             error(getStringFormat("error_om_not_found", buildingCode.getBuildingCode()));
                             return;
                         }
@@ -191,9 +190,28 @@ public class HeatmeterEdit extends FormTemplatePage{
 
                         //update om for periods
                         List<HeatmeterPeriod> periods = heatmeter.getPeriods();
-                        for (HeatmeterPeriod p : periods){
+                        for (HeatmeterPeriod p : periods) {
                             p.setBeginOm(om);
                         }
+                    }
+
+                    //adjust begin date for previous inputs
+                    {
+                        int size = heatmeter.getInputs().size();
+                        HeatmeterInput prev = null;
+                        for (HeatmeterInput input : heatmeter.getInputs()) {
+                            if (prev != null) {
+                                input.setBeginDate(nextDay(prev.getEndDate()));
+                            } else if (heatmeter.getId() == null || size == 1) {
+                                input.setBeginDate(heatmeter.getOm());
+                            }
+                            prev = input;
+                        }
+                    }
+
+                    //recalculate consumptions for inputs
+                    {
+                        heatmeterService.calculateConsumptions(heatmeter.getPayloads(), heatmeter.getInputs());
                     }
 
                     //save
@@ -202,14 +220,16 @@ public class HeatmeterEdit extends FormTemplatePage{
                     getSession().info(getStringFormat("info_saved", heatmeter.getLs()));
                 } catch (Exception e) {
                     log.error("Ошибка сохранения теплосчетчика", e);
-                    getSession().error(new AbstractException(e, "Ошибка сохранения теплосчетчика: {0}", model.getObject().getLs()) {});
+                    getSession().error(new AbstractException(e, "Ошибка сохранения теплосчетчика: {0}", model.getObject().getLs()) {
+                    });
                 }
 
                 setResponsePage(HeatmeterList.class);
             }
         });
 
-        form.add(new Link("cancel"){
+        form.add(new Link("cancel") {
+
             @Override
             public void onClick() {
                 setResponsePage(HeatmeterList.class);
